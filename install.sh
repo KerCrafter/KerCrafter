4a6e8192b8dad047a5f055e47ca8ae8f17763773
KerCrafter <clemkerneur@gmail.com>
2024-12-04 23:48:22 +0100

    install.sh should be re-execuded to updating

--- install.sh
+++ install.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 
-git clone https://github.com/KerCrafter/KerCrafter.git ~/.kercrafter_tools
-echo "# KerCrafter tools" >> ~/.bashrc 
-echo "source ~/.kercrafter_tools/.bashrc" >> ~/.bashrc 
+if [ -d ~/.kercrafter_tools ] 
+then
+	cd ~/.kercrafter_tools
+	git pull
+else
+	git clone https://github.com/KerCrafter/KerCrafter.git ~/.kercrafter_tools
+	echo "# KerCrafter tools" >> ~/.bashrc 
+	echo "source ~/.kercrafter_tools/.bashrc" >> ~/.bashrc 
+fi
 
-source ~/.kercrafter_tools/.bashrc
+. ~/.kercrafter_tools/.bashrc
 
 #install VIM Colorscheme
-mkdir -p ~/.vim/colors 
-curl -o ~/.vim/colors/one.vim https://raw.githubusercontent.com/rakr/vim-one/refs/heads/master/colors/one.vim
-
+if [ ! -d ~/.vim/colors ] 
+then
+	mkdir -p ~/.vim/colors 
+	curl -o ~/.vim/colors/one.vim https://raw.githubusercontent.com/rakr/vim-one/refs/heads/master/colors/one.vim
+fi
 
 #install vim surround
-mkdir -p ~/.vim/pack/tpope/start
-cd ~/.vim/pack/tpope/start
-git clone https://tpope.io/vim/surround.git
-vim -u NONE -c "helptags surround/doc" -c q
+if [ ! -d ~/.vim/pack/tpope/start ] 
+then
+	echo "Install VimSurround"
+	mkdir -p ~/.vim/pack/tpope/start
+	cd ~/.vim/pack/tpope/start
+	git clone https://tpope.io/vim/surround.git
+	vim -u NONE -c "helptags surround/doc" -c q
+fi
